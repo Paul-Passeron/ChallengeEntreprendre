3824983565c3e23a337dcd9e4d3705e5d7400b94
paulpasseron <paul.passeron@ensiie.eu>
2024-11-04 15:55:12 +0100

    Added account.html

--- src/app.py
+++ src/app.py
@@ -8,6 +8,16 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
-@app.route('/profile')
-def profile():
-  return render_template('profile.html')
+
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+def fib(n):
+  if n < 2: return 1
+  return fib(n - 1) + fib(n-2)
+
+@app.route('/account/')
+@app.route('/account/<name>')
+def account(name=None):
+  return render_template('account.html', person=name)
