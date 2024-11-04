9bb8d3dd1274e733a7ae071968dbb0a4b883fbbd
paulpasseron <paul.passeron@ensiie.eu>
2024-11-04 15:07:48 +0100

    First run

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## Liens
 
 - [Google Docs](https://docs.google.com/document/d/1uHUpylgCI_WJFU-5yH4fgMxVcnJcHAK6U0iv2w1kVJQ/edit?usp=sharing)
+
+## Commande
+```flask --app app run```
--- src/app.py
+++ src/app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.config.from_object('config')
+# app.config.from_object('config')
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
+@app.route('/profile')
+def profile():
+  return render_template('profile.html')
